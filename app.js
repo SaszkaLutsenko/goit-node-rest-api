3b9002ae07cf33ecfc26b9054af2e3547bee0691
Oleksandr Lutsenko <sashalutsenko1991@gmail.com>
2024-05-08 09:14:49 +0200

    в

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+import 'dotenv/config';
+
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import mongoose from "mongoose";
 
 import contactsRouter from "./routes/contactsRouters.js";
+import "./db/db.js";
 
 const app = express();
 
@@ -11,6 +15,19 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
+
+const DB_URI = process.env.DB_URI;
+
+
+export const run = async (err, req, res, next) => {
+  try {
+      await mongoose.connect(DB_URI)
+      console.log("Database connection successful")
+  } catch(error) {
+      next(error);
+     
+  }
+};
 
 app.use((_, res) => {
   res.status(404).json({ message: "Route not found" });
